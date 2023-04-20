652e2f991865742e5b962f1fa3c04d19f8e7f266
gushi-cookie <gushi-cookie@protonmail.com>
2023-04-21 01:28:58 +0300

    Style the app according to the design's layout.

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,53 @@ import { RouterView } from 'vue-router'
 
 <template>
   <Header/>
-  <Sidebar/>
-  <RouterView/>
+  <div class="app__wrap">
+    <Sidebar/>
+    <RouterView class="main" />
+  </div>
   <Footer/>
   <Modal/>
 </template>
 
 <style>
+#app {
+  display: flex;
+  flex-flow: column nowrap;
 
+  height: 100vh;
+}
+
+.app__wrap {
+  display: flex;
+  flex-flow: row nowrap;
+
+  min-height: 0px;
+  z-index: 1;
+}
+.sidebar {
+  flex-shrink: 0;
+}
+.main {
+  overflow-x: hidden;
+  flex-grow: 1;
+}
+.main::-webkit-scrollbar {
+    width: 9px;
+}
+.main::-webkit-scrollbar-thumb {
+    background-color: #f99003;
+    border-radius: 3px;
+}
+.main::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 3px 0.08px grey;
+}
+
+.header {
+  z-index: 2;
+  flex-shrink: 0;
+}
+.footer {
+  z-index: 1;
+  flex-shrink: 0;
+}
 </style>
