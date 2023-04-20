ee8b7bb02bf5a3af5e286fdf4b6b19997f08739b
gushi-cookie <gushi-cookie@protonmail.com>
2023-04-20 17:21:59 +0300

    Create Header component. Style LocaleSettings and Header components.

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import LocaleSettings from './LocaleSettings.vue'
+import { RouterLink } from 'vue-router'
+import { sidebarState } from '@/services/SidebarState'
+</script>
+
+<template>
+    <header class="header">
+        <div class="header__btn-toggle" @click="sidebarState.isShrink = !sidebarState.isShrink">
+            <img src="/icons/menu.svg" :class="{ 'd-none': !sidebarState.isShrink }">
+            <img src="/icons/round-close.svg" :class="{ 'd-none': sidebarState.isShrink }">
+        </div>
+        <div class="header__logo">
+            <RouterLink to="/">News Stock</RouterLink>
+        </div>
+        <nav class="header__nav">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </nav>
+        <LocaleSettings/>
+    </header>
+</template>
+
+<style>
+.header {
+    display: flex;
+    background-color: #1976d2;
+    height: 64px;
+    color: #fff;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+}
+
+.header__btn-toggle {
+    cursor: pointer;
+    padding: 8px;
+    margin-left: 24px;
+    margin-right: 25px;
+}
+.header__btn-toggle img {
+    display: block;
+    width: 28px;
+    filter: invert(1);
+}
+
+.header__logo a {
+    color: #fff;
+    text-decoration: none;
+    padding: 8px;
+    font-weight: 700;
+}
+.header__logo a:hover {
+    color: #a2d1ff;
+}
+
+.header__nav {
+    border-left: 1px solid #fff;
+    margin-left: 10px;
+}
+.header__nav a {
+    color: #fff;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+.header__nav a:hover {
+    color: #a2d1ff;
+}
+
+.header .locale-settings {
+    margin-left: auto;
+    margin-right: 20px;
+}
+</style>
--- src/components/LocaleSettings.vue
+++ src/components/LocaleSettings.vue
@@ -5,7 +5,7 @@ import { getCurrentLocale } from '@/services/LocaleService'
 
 <template>
     <div class="locale-settings" @click="openModal('Select search language.', 'LocaleSelect')">
-        <span class="locale-settings__lang">Language:</span>
+        <span class="locale-settings__lang">Language</span>
         <span class="locale-settings__icon" v-html="getCurrentLocale().icon"></span>
     </div>
 </template>
@@ -13,12 +13,29 @@ import { getCurrentLocale } from '@/services/LocaleService'
 <style>
 .locale-settings {
     cursor: pointer;
+    padding: 5px;
+    border: 1px solid #a2d1ff;
+    border-radius: 6px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
 }
+.locale-settings:hover {
+    opacity: 0.75;
+}
+
 .locale-settings__lang {
     margin-right: 5px;
 }
 .locale-settings__icon {
     display: inline-block;
+    box-sizing: content-box;
     width: 14px;
+    height: 14px;
+    border: 3px solid #e6e6e6;
+    border-radius: 6px;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+    vertical-align: -5px;
+}
+.locale-settings__icon > svg {
+    vertical-align: top;
 }
 </style>
