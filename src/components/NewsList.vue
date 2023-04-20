74ff6f084ec812fe8e43ddda109dbaf70de4163a
gushi-cookie <gushi-cookie@protonmail.com>
2023-04-20 23:50:22 +0300

    Move GDELT API functional from NewsList component to GDELTProjectAPI.ts service.

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,89 +1,15 @@
-<script lang="ts">
-import axios, { AxiosError } from 'axios'
-import type { AxiosResponse } from 'axios'
+<script setup lang="ts">
+import NewsItem from './NewsItem.vue'
 import type { Props as NewsItemProps } from './NewsItem.vue'
+import { onMounted, ref, type Ref } from 'vue'
+import GDELTProjectAPI, { type Article } from '@/services/GDELTProjectAPI'
 
-const requiredResponseFields: string[] = ['title', 'sourcecountry', 'language', 'seendate', 'url', 'domain', 'socialimage'];
+const newsProps: Ref<NewsItemProps[]> = ref([]);
 
-/**
- * Return true if some of fields in the obj are undefined, null, or 0 length string.
- * @param obj object to inspect.
- * @param fields fields to be inspected in the object.
- */
-function checkEmptyFields(obj: any, fields: string[]): boolean {
-    let objectFields = Object.getOwnPropertyNames(obj);
-
-    for(let i = 0; i < fields.length; i++) {
-        if(!objectFields.includes(fields[i]) ||
-           obj[fields[i]] === null ||
-           obj[fields[i]] === undefined ||
-           (typeof obj[fields[i]] === 'string' && obj[fields[i]].length === 0) ||
-           (obj[fields[i]] instanceof String && obj[fields[i]].length === 0)
-        ) return true;
-    }
-    return false;
-}
-
-interface ResponseArticle {
-    domain: string,
-    language: string,
-    seendate: string,
-    socialimage: string,
-    sourcecountry: string,
-    title: string,
-    url: string,
-    url_mobile: string,
-}
-
-/**
- * Check if an object is instance of the ResponseArticle interface.
- * @param obj 
- */
-function isResponseArticle(obj: any): boolean {
-    return Object.prototype.hasOwnProperty.call(obj, 'domain') &&
-    Object.prototype.hasOwnProperty.call(obj, 'language') &&
-    Object.prototype.hasOwnProperty.call(obj, 'seendate') &&
-    Object.prototype.hasOwnProperty.call(obj, 'socialimage') &&
-    Object.prototype.hasOwnProperty.call(obj, 'sourcecountry') &&
-    Object.prototype.hasOwnProperty.call(obj, 'title') &&
-    Object.prototype.hasOwnProperty.call(obj, 'url_mobile');
-}
-
-class InvalidGDELTResponseError extends Error {
-    response: AxiosResponse;
-
-    constructor(message: string, response: AxiosResponse) {
-        super(message);
-        this.response = response;
-    };
-};
-
-/**
- * Make https request to the GDELT api, with options.
- * @param searchPrase search phrase.
- * @param languageCode ISO-639-2/T code of a language.
- * @param countryCode ISO-3166-1/Alpha-2 code of a country.
- * @throws { AxiosError, InvalidGDELTResponseError }
- */
-async function fetchNews(searchPhrase: string, languageCode: string | null, countryCode: string | null): Promise<NewsItemProps[]> {
-    let params = new URLSearchParams();
-    languageCode = languageCode ? ` sourcelang:${languageCode}` : '';
-    countryCode = countryCode ? ` sourcecountry:${countryCode}` : '';
-    params.append('query', `${searchPhrase}${languageCode}${countryCode}`);
-    params.append('format', 'json');
-
-    let result = await axios.get(`https://api.gdeltproject.org/api/v2/doc/doc`, { params });
-    if(!result.data || !result.data.articles) {
-        throw new InvalidGDELTResponseError('Couldn\'t find data.articles property in the response object.', result);
-    }
-
-    let props: NewsItemProps[] = [];
-    let articles: ResponseArticle[] = result.data.articles;
+function toNewsItemProps(articles: Article[]): NewsItemProps[] {
+    let result: NewsItemProps[] = [];
     for(let i = 0; i < articles.length; i++) {
-        if(!isResponseArticle(articles[i]) || checkEmptyFields(articles[i], requiredResponseFields))
-            continue;
-
-        props.push({
+        result.push({
             domain: articles[i].domain,
             imageLink: articles[i].socialimage,
             language: articles[i].language,
@@ -92,19 +18,11 @@ async function fetchNews(searchPhrase: string, languageCode: string | null, coun
             title: articles[i].title,
         });
     }
-    return props;
+    return result;
 }
-</script>
-
-<script setup lang="ts">
-import NewsItem from './NewsItem.vue'
-import { onMounted, ref } from 'vue'
-import type { Ref } from 'vue'
-
-const newsProps: Ref<NewsItemProps[]> = ref([]);
 
 onMounted(async () => {
-    newsProps.value.push(...await fetchNews('Television', null, null));
+    newsProps.value.push(...toNewsItemProps(await GDELTProjectAPI.fetchNews('Television', null, null)));
 });
 </script>
 
---
+++ src/services/GDELTProjectAPI.ts
@@ -0,0 +1,98 @@
+import axios, { type AxiosResponse } from "axios";
+
+
+const requiredResponseFields: string[] = ['title', 'sourcecountry', 'language', 'seendate', 'url', 'domain', 'socialimage'];
+
+
+class InvalidGDELTResponseError extends Error {
+    response: AxiosResponse;
+    constructor(message: string, response: AxiosResponse) {
+        super(message);
+        this.response = response;
+    };
+}
+
+
+export interface Article {
+    domain: string,
+    language: string,
+    seendate: string,
+    socialimage: string,
+    sourcecountry: string,
+    title: string,
+    url: string,
+    url_mobile: string,
+}
+
+
+/**
+ * Check if an object is instance of the Article interface.
+ * @param obj 
+ */
+function isArticle(obj: any): boolean {
+    return Object.prototype.hasOwnProperty.call(obj, 'domain') &&
+    Object.prototype.hasOwnProperty.call(obj, 'language') &&
+    Object.prototype.hasOwnProperty.call(obj, 'seendate') &&
+    Object.prototype.hasOwnProperty.call(obj, 'socialimage') &&
+    Object.prototype.hasOwnProperty.call(obj, 'sourcecountry') &&
+    Object.prototype.hasOwnProperty.call(obj, 'title') &&
+    Object.prototype.hasOwnProperty.call(obj, 'url_mobile');
+}
+
+
+/**
+ * Return true if some of fields in the obj are undefined, null, or 0 length string.
+ * @param obj object to inspect.
+ * @param fields fields to be inspected in the object.
+ */
+function checkEmptyFields(obj: any, fields: string[]): boolean {
+    let objectFields = Object.getOwnPropertyNames(obj);
+
+    for(let i = 0; i < fields.length; i++) {
+        if(!objectFields.includes(fields[i]) ||
+           obj[fields[i]] === null ||
+           obj[fields[i]] === undefined ||
+           (typeof obj[fields[i]] === 'string' && obj[fields[i]].length === 0) ||
+           (obj[fields[i]] instanceof String && obj[fields[i]].length === 0)
+        ) return true;
+    }
+    return false;
+}
+
+
+/**
+ * Make https request to GDELT api, with options.
+ * @param searchPrase search phrase.
+ * @param languageCode ISO-639-2/T code of a language.
+ * @param countryCode ISO-3166-1/Alpha-2 code of a country.
+ * @throws { AxiosError, InvalidGDELTResponseError }
+ */
+async function fetchNews(searchPhrase: string, languageCode: string | null, countryCode: string | null): Promise<Article[]> {
+    let params = new URLSearchParams();
+    languageCode = languageCode ? ` sourcelang:${languageCode}` : '';
+    countryCode = countryCode ? ` sourcecountry:${countryCode}` : '';
+    params.append('query', `${searchPhrase}${languageCode}${countryCode}`);
+    params.append('format', 'json');
+
+    let result = await axios.get(`https://api.gdeltproject.org/api/v2/doc/doc`, { params });
+    if(!result.data || !result.data.articles) {
+        throw new InvalidGDELTResponseError('Couldn\'t find data.articles property in the response object.', result);
+    }
+
+    let completeArticles: Article[] = [];
+    let articles: Article[] = result.data.articles;
+    for(let i = 0; i < articles.length; i++) {
+        if(isArticle(articles[i]) && !checkEmptyFields(articles[i], requiredResponseFields)) {
+            completeArticles.push(articles[i]);
+        }
+    }
+    return completeArticles;
+}
+
+
+export default {
+    isArticle,
+    checkEmptyFields,
+    fetchNews,
+    InvalidGDELTResponseError,
+}
