c047a5188382d9c4afab76ff8d8617b49330d39d
gushi-cookie <gushi-cookie@protonmail.com>
2023-04-21 00:21:13 +0300

    Create Footer component.

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="footer">
+        <p class="footer__text">Made with <img src="/icons/vue.svg"> by <a href="https://github.com/gushi-cookie" target="_blank">Gushi</a></p>
+    </footer>
+</template>
+
+<style>
+.footer {
+    background-color: #424242;
+    color: #fff;
+    width: 100%;
+    height: 36px;
+    font-size: 0.86em;
+}
+
+.footer__text {
+    font-weight: 500;
+    margin: 0;
+    padding: 9px 0 0 18px;
+}
+.footer__text img {
+    width: 1.4em;
+    vertical-align: -3px;
+}
+.footer__text a {
+    color: #fff !important;
+}
+</style>
