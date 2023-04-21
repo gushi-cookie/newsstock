9b03fdbc2c7670b9a083e370619ce89058f4f3f8
gushi-cookie <926396z@mail.ru>
2023-04-21 03:18:15 +0300

    Style NotFoundView component.

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,15 @@
 <template>
-    <main>
+    <main class="main__not-found">
         <h1>404 Page not found!</h1>
     </main>
 </template>
+
+<style>
+.main__not-found {
+    text-align: center;
+}
+
+.main__not-found h1 {
+    margin-top: 20%;
+}
+</style>
