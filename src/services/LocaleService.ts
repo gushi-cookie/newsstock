3be315c4f88ab5ed4945d1ddbc16599fd0943fcc
gushi-cookie <926396z@mail.ru>
2023-04-23 14:56:09 +0300

    Create functional for selecting locales and storing them in the cookies.

--- src/services/LocaleService.ts
+++ src/services/LocaleService.ts
@@ -1,13 +1,21 @@
 import * as Flags from 'country-flag-icons/string/1x1'
+import Cookies from 'js-cookie'
+import { ref } from 'vue'
 
 export interface Locale {
     name: string,
     languageCode: string,
     countryCode: string,
     icon: string,
-};
+}
 
-const locales: Locale[] = [
+export const locale = ref<Locale>();
+
+function anyIcon(): string {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19.5 7A9 9 0 0 0 12 3a8.991 8.991 0 0 0-7.484 4"/><path d="M11.5 3a16.989 16.989 0 0 0-1.826 4M12.5 3a16.989 16.989 0 0 1 1.828 4M19.5 17a9 9 0 0 1-7.5 4a8.991 8.991 0 0 1-7.484-4"/><path d="M11.5 21a16.989 16.989 0 0 1-1.826-4m2.826 4a16.989 16.989 0 0 0 1.828-4M2 10l1 4l1.5-4L6 14l1-4m10 0l1 4l1.5-4l1.5 4l1-4M9.5 10l1 4l1.5-4l1.5 4l1-4"/></g></svg>`;
+}
+export const locales: Locale[] = [
+    { languageCode: 'any', countryCode: 'any', icon: anyIcon(), name: 'Search everywhere' },
     { languageCode: 'spa', countryCode: 'AR', icon: Flags.AR, name: 'Argentina' },
     { languageCode: 'en', countryCode: 'AS', icon: Flags.AU, name: 'Australia' },
     { languageCode: 'deu', countryCode: 'AU', icon: Flags.AT, name: 'Austria' },
@@ -47,8 +55,53 @@ const locales: Locale[] = [
 
 export function getLocales(): Locale[] {
     return locales;
-};
+}
 
-export function getCurrentLocale(): Locale {
-    return { languageCode: 'en', countryCode: 'UK', icon: Flags.GB, name: 'United Kingdom' };
-};
+export function getLocale(countryCode: string, languageCode: string): Locale | null {
+    for(let i = 0; i < locales.length; i++) {
+        if(locales[i].countryCode === countryCode && locales[i].languageCode === languageCode) return locales[i];
+    }
+    return null;
+}
+
+export function getDefaultLocale(): Locale {
+    return locales[0];
+}
+
+/**
+ * Set default locale to the cookies.
+ */
+export function setDefaultLocale() {
+    let defaultLocale = getDefaultLocale();
+    Cookies.set('locale', `${defaultLocale.countryCode};${defaultLocale.languageCode}`);
+    locale.value = defaultLocale;
+}
+
+/**
+ * Set a specific locale to the cookies and locale constant.
+ * @param newLocale 
+ */
+export function setLocale(newLocale: Locale) {
+    Cookies.set('locale', `${newLocale.countryCode};${newLocale.languageCode}`);
+    locale.value = newLocale;
+}
+
+/**
+ * Get a locale stored in the cookies.
+ * @returns Null if not set.
+ */
+export function getStoredLocale(): Locale | null {
+    let storedLocale = Cookies.get('locale');
+    if(!storedLocale) return null;
+    storedLocale = storedLocale.split(';');
+    if(storedLocale.length !== 2) return null;
+    return getLocale(storedLocale[0], storedLocale[1]);
+}
+
+
+let storedLocale = getStoredLocale();
+if(storedLocale) {
+    setLocale(storedLocale);
+} else {
+    setDefaultLocale();
+}
